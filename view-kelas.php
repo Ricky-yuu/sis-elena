672b4cd8980c9deb2180cea02e0449af1cd35cf2
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2020-11-27 08:45:34 +0700

    menampilkan kelas di dasboard guru sesuai dengan kode mapel dan kode kelas

--- view-kelas.php
+++ view-kelas.php
@@ -1,30 +1,27 @@
 <?php
   include 'templates/header-guru.php';
   $id = $_GET['id'];
-
+  $kodeMapelGuru = $rowUser['kode_mapel'];
+  $tampilMateriQuery = mysqli_query($link, "SELECT * FROM materi_mapel where kode_kelas='$id' and kode_mapel='$kodeMapelGuru'");
+  //$tampilMateri = mysqli_fetch_array($tampilMateriQuery);
 ?>
 
     <!-- main start-->
     <div class="container">
       <div class="row row-cols-1 row-cols-md-1">
+        <?php
+          while ($tampilMateri = mysqli_fetch_array($tampilMateriQuery)) :
+        ?>
         <div class="col mb-4">
           <div class="card">
               <div class="card-body">
-                <a href="#" class="text-color-a"><h5 class="card-title">Bab 1 Aljabar</h5></a>
+                <a href="#" class="text-color-a"><h5 class="card-title"><?php echo $tampilMateri['judul']; ?></h5></a>
                 <p class="card-text">Presensi, 2 Januari 2020</p>
                 <p class="card-text">Materi Pengantar Aljabar</p>
               </div>
           </div>
         </div>
-        <div class="col mb-4">
-          <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Bab 1 Aljabar</h5>
-                <p class="card-text">Presensi, 2 Januari 2020</p>
-                <p class="card-text">Materi Pengantar Aljabar</p>
-              </div>
-          </div>
-        </div>
+      <?php endwhile; ?>
       </div>
     </div>
     <!-- main end-->
