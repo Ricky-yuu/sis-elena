d1806580dae14df467bc6201847124b3709d733e
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2020-12-01 13:49:04 +0700

    menambahkan tampilan view presensi

---
+++ view-presensi.php
@@ -0,0 +1,52 @@
+<?php
+  include 'templates/header.php';
+?>
+
+    <!-- main start-->
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-1">
+        <div class="col mb-4">
+          <div class="card-deck">
+            <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Bab 1 Aljabar</h5>
+                  <p class="card-text">Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.</p>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card-deck">
+            <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Presensi</h5>
+                  <div class="card bg-transparent border-0" style="width: 25rem;">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">Tanggal</th>
+                          <th scope="col">Status</th>
+                          <th scope="col">Aksi</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>2 Januari 2021</td>
+                          <td>Selesai</td>
+                          <td><a href="#" class="text-color-a">Tambahkan Tugas</a></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div class="col text-center">
+                    <a href="#" class="btn btn-success" style="width: 10rem;" >Tambahkan Tugas</a>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- main end-->
+
+<?php include 'templates/footer.php' ?>
