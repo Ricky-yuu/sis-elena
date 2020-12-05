a8c17b030cf06b93e56c906a4ea3d4babe4a2fe4
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2020-12-05 19:40:17 +0700

    perbaikan bug dan perubahan syntax sql

--- view-kelas.php
+++ view-kelas.php
@@ -5,8 +5,7 @@
   $kodeMapelGuruQuery = mysqli_query($link, "SELECT * FROM tb_mengajar where kode_guru='$kodeGuru' and kode_kelas='$id'");
   $kodeMapelGuru = mysqli_fetch_array($kodeMapelGuruQuery);
   $kodeMapelGuru2 = $kodeMapelGuru['kode_mapel'];
-  $tampilMateriQuery = mysqli_query($link, "SELECT * FROM materi_mapel where kode_kelas='$id' and kode_mapel='$kodeMapelGuru2'");
-  //$tampilMateri = mysqli_fetch_array($tampilMateriQuery);
+  $tampilMateriQuery = mysqli_query($link, "SELECT materi_mapel.judul, materi_mapel.kode_aktivitas, materi_mapel.kode_aktivitas2, materi_mapel.kode_aktivitas3, presensi2.nama_presensi, tugas2.nama_tugas FROM materi_mapel LEFT JOIN presensi2 ON materi_mapel.kode_aktivitas=presensi2.kode_aktivitas LEFT JOIN tugas2 ON materi_mapel.kode_aktivitas2=tugas2.kode_aktivitas2 where materi_mapel.kode_mapel='$kodeMapelGuru2' AND materi_mapel.kode_kelas='$id'");
 ?>
 
     <!-- main start-->
@@ -20,8 +19,8 @@
           <div class="card">
               <div class="card-body">
                 <a href="#" class="text-color-a"><h5 class="card-title"><?php echo $tampilMateri['judul']; ?></h5></a>
-                <p class="card-text">Presensi, 2 Januari 2020</p>
-                <p class="card-text">Materi Pengantar Aljabar</p>
+                <a href="view-presensi-guru.php?id=<?php echo $tampilMateri['kode_aktivitas'];?>&idm=<?php echo $id; ?>" class="card-text text-color-a"><p><?php echo $tampilMateri['nama_presensi']; ?></p></a>
+                <a href="" class="card-text text-color-a"><p><?php echo $tampilMateri['nama_tugas']; ?></p></a>
               </div>
           </div>
         </div>
